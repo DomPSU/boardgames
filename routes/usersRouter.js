57b974a1dd1950c4bb40119be6192a13a24f65f5
Dominic Lupo <DominicLupo5318@gmail.com>
2020-11-29 20:03:18 -0500

    change id to userID within users router

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -9,23 +9,23 @@ const usersRouter = express.Router();
 usersRouter.use(onlyReturnsJSON);
 
 // get
-usersRouter.get("/:id", usersController.show);
+usersRouter.get("/:userID", usersController.show);
 usersRouter.get("/", usersController.index);
 
 // post
-usersRouter.post("/:id", methodNotAllowed);
+usersRouter.post("/:userID", methodNotAllowed);
 usersRouter.post("/", usersController.create);
 
 // put
-usersRouter.put("/:id", methodNotAllowed);
+usersRouter.put("/:userID", methodNotAllowed);
 usersRouter.put("/", methodNotAllowed);
 
 // patch
-usersRouter.patch("/:id", methodNotAllowed);
+usersRouter.patch("/:userID", methodNotAllowed);
 usersRouter.patch("/", methodNotAllowed);
 
 // delete
-usersRouter.delete("/:id", methodNotAllowed);
+usersRouter.delete("/:userID", methodNotAllowed);
 usersRouter.delete("/", methodNotAllowed);
 
 module.exports = usersRouter;
